9c8d74c70354a1a1e116add67914e06d67609f36
harsh vijay <iharsh234@gmail.com>
2015-07-11 23:03:00 +0530

    Update Quiz-Problem4.py

--- Quiz-Problem4.py
+++ Quiz-Problem4.py
@@ -1,4 +1,9 @@
 # PROBLEM 4  
+#Write a simple procedure, myLog(x, b), that computes the logarithm of a number x relative to a base b. For example,
+#if x = 16 and b = 2, then the result is 4 - because 24=16. If x = 15 and b = 3,
+#then the result is 2 - because 32 is the largest power of 3 less than 15.
+#In other words, myLog should return the largest power of b such that b to that power is still less than or equal to x.
+#x and b are both positive integers; b is an integer greater than or equal to 2. Your function should return an integer answer
 
 def myLog(x,b):
    c = [x,b]
